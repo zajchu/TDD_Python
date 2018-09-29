54c2cf006a8593f2c9f480e7fce2a920069c1f06
Aleksander Zajchowski <azajchowski@groupon.com>
2018-09-29 14:42:59 +0200

    Function test and list add

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -11,10 +12,24 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('paceholder'),
+                         'Wpisz rzeczy do zrobienia')
+
+        input_box.send_keys('Kupic pawie piora')
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Kupic pawie piora' for row in rows))
+
         self.fail("Zakończenie testu")
 
 
