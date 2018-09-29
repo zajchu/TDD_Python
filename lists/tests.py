f6782ee9b449491b0d0605fe56ca360b7d8a104f
Aleksander Zajchowski <azajchowski@groupon.com>
2018-09-29 08:15:32 +0200

    Fixed unittest

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
@@ -9,3 +11,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_views(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertTrue(response.content.startswith(b"<html>"))
+        self.assertIn(b"<title>Listy rzeczy do zrobienia</title>", response.content)
+        self.assertTrue(response.content.endswith(b"</html>"))
