902ebac195eb0fadb1300d61e79c304676998583
Aleksander Zajchowski <azajchowski@groupon.com>
2018-10-01 10:05:20 +0200

    testing string template

--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
+    return render(request, 'home.html')
