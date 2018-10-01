fb7ac7a0cf5833f935f3e1adc6482aee4a65f585
Aleksander Zajchowski <azajchowski@groupon.com>
2018-10-01 17:05:41 +0200

    create functional_test directory

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('Listy', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -42,6 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         self.check_row_for_in_list_table('2: Uzyc pawich pior do zrobienia przynety')
 
         self.fail("Zakończenie testu")
+
+
 
 
 if __name__ == '__main__':
